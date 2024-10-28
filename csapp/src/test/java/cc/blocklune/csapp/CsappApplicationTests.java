5fe97d4c29c7e8fe8b604b248ce49e6cb7f1c256
BlockLune <39331194+BlockLune@users.noreply.github.com>
2024-10-28 11:20:49 +0800

    feat: backend init commit

---
+++ csapp/src/test/java/cc/blocklune/csapp/CsappApplicationTests.java
@@ -0,0 +1,13 @@
+package cc.blocklune.csapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
