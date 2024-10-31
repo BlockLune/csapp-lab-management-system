f07abab037316e8fceecbfa21b252fe893011608
BlockLune <39331194+BlockLune@users.noreply.github.com>
2024-10-31 19:50:35 +0800

    build(deps): add framer motion

--- management-system-frontend/components/login-form.tsx
+++ management-system-frontend/components/login-form.tsx
@@ -1,11 +1,13 @@
 'use client';
 
+import { useState } from "react";
 import { useRouter } from 'next/navigation';
 import { Card, Flex, Heading, Text, TextField, Button } from "@radix-ui/themes";
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 import { login } from '@/scripts/api';
 import { getRouteFromRole } from '@/scripts/auth';
+import * as motion from "framer-motion/client"
 
 const validationSchema = Yup.object().shape({
     username: Yup.string().required('Username is required'),
@@ -14,51 +16,59 @@ const validationSchema = Yup.object().shape({
 
 export default function LoginForm() {
     const router = useRouter();
+    const [loading, setLoading] = useState(false);
     return (
-        <Formik
-            initialValues={{ username: '', password: '' }}
-            validationSchema={validationSchema}
-            onSubmit={async (values) => {
-                const data = await login(values.username, values.password);
-                if (data) {
-                    router.push(getRouteFromRole(data.roles));
-                } else {
-                    alert('Invalid username or password');
-                }
-            }}
+        <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
         >
-            {({ handleSubmit }) => (
-                <Form onSubmit={handleSubmit}>
-                    <Card variant="classic">
-                        <Flex gap="5" direction="column" align="start" p="4" width="24rem">
-                            <Heading as="h2">Sign in</Heading>
-                            <Flex direction="column" gap="3" width="100%">
-                                <Flex justify="between" align="center">
-                                    <Text as="label" htmlFor="username">Username</Text>
-                                    <ErrorMessage name="username" component="div" className="text-red-11 text-xs" />
+            <Formik
+                initialValues={{ username: '', password: '' }}
+                validationSchema={validationSchema}
+                onSubmit={async (values) => {
+                    setLoading(true);
+                    const data = await login(values.username, values.password);
+                    if (data) {
+                        router.push(getRouteFromRole(data.roles));
+                    } else {
+                        alert('Invalid username or password');
+                    }
+                    setLoading(false);
+                }}
+            >
+                {({ handleSubmit }) => (
+                    <Form onSubmit={handleSubmit}>
+                        <Card variant="classic">
+                            <Flex gap="5" direction="column" align="start" p="4" width="24rem">
+                                <Heading as="h2">Sign in</Heading>
+                                <Flex direction="column" gap="3" width="100%">
+                                    <Flex justify="between" align="center">
+                                        <Text as="label" htmlFor="username">Username</Text>
+                                        <ErrorMessage name="username" component="div" className="text-red-11 text-xs" />
+                                    </Flex>
+                                    <Field name="username">
+                                        {({ field }: { field: any }) => (
+                                            <TextField.Root id="username" {...field} placeholder="Enter your username" />
+                                        )}
+                                    </Field>
+                                    <Flex justify="between" align="center">
+                                        <Text as="label" htmlFor="password">Password</Text>
+                                        <ErrorMessage name="password" component="div" className="text-red-11 text-xs" />
+                                    </Flex>
+                                    <Field name="password">
+                                        {({ field }: { field: any }) => (
+                                            <TextField.Root id="password" type="password" {...field} placeholder="Enter your password" />
+                                        )}
+                                    </Field>
                                 </Flex>
-                                <Field name="username">
-                                    {({ field }: { field: any }) => (
-                                        <TextField.Root id="username" {...field} placeholder="Enter your username" />
-                                    )}
-                                </Field>
-                                <Flex justify="between" align="center">
-                                    <Text as="label" htmlFor="password">Password</Text>
-                                    <ErrorMessage name="password" component="div" className="text-red-11 text-xs" />
+                                <Flex gap="2" width="100%">
+                                    <Button loading={loading} type="submit" variant="solid" style={{ flex: 1, cursor: "pointer" }}>Sign in</Button>
                                 </Flex>
-                                <Field name="password">
-                                    {({ field }: { field: any }) => (
-                                        <TextField.Root id="password" type="password" {...field} placeholder="Enter your password" />
-                                    )}
-                                </Field>
                             </Flex>
-                            <Flex gap="2" width="100%">
-                                <Button type="submit" variant="solid" style={{ flex: 1, cursor: "pointer" }}>Sign in</Button>
-                            </Flex>
-                        </Flex>
-                    </Card>
-                </Form>
-            )}
-        </Formik>
+                        </Card>
+                    </Form>
+                )}
+            </Formik>
+        </motion.div>
     );
 }
