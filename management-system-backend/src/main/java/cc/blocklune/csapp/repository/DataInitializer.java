ca0ad039508099e242876665edf28f3e44e98da3
BlockLune <39331194+BlockLune@users.noreply.github.com>
2024-10-31 09:19:05 +0800

    feat: data initializer (lab info)

--- management-system-backend/src/main/java/cc/blocklune/csapp/repository/DataInitializer.java
+++ management-system-backend/src/main/java/cc/blocklune/csapp/repository/DataInitializer.java
@@ -1,38 +1,52 @@
 package cc.blocklune.csapp.repository;
 
+import cc.blocklune.csapp.model.LabInfo;
+import cc.blocklune.csapp.model.SystemUser;
+import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
-import cc.blocklune.csapp.model.SystemUser;
-
-import java.util.Set;
 
 @Component
 public class DataInitializer implements CommandLineRunner {
 
-    @Autowired
-    private SystemUserRepository userRepository;
+  @Autowired
+  private SystemUserRepository userRepository;
+  @Autowired
+  private LabInfoRepository labInfoRepository;
+  @Autowired
+  private PasswordEncoder passwordEncoder;
 
-    @Autowired
-    private PasswordEncoder passwordEncoder;
+  @Override
+  public void run(String... args) throws Exception {
+    if (userRepository.count() == 0) {
+      SystemUser teacher = new SystemUser();
+      teacher.setUsername("the-teacher");
+      teacher.setPassword(passwordEncoder.encode("the-teacher-password"));
+      teacher.setRoles(Set.of("TEACHER"));
+      userRepository.save(teacher);
 
-    @Override
-    public void run(String... args) throws Exception {
-        if (userRepository.count() == 0) {
-            SystemUser teacher = new SystemUser();
-            teacher.setUsername("the-teacher");
-            teacher.setPassword(passwordEncoder.encode("the-teacher-password"));
-            teacher.setRoles(Set.of("TEACHER"));
-            userRepository.save(teacher);
-
-            for (int i = 0; i < 10; ++i) {
-                SystemUser student = new SystemUser();
-                student.setUsername("student-" + i);
-                student.setPassword(passwordEncoder.encode("student-" + i + "-password"));
-                student.setRoles(Set.of("STUDENT"));
-                userRepository.save(student);
-            }
-        }
+      for (int i = 0; i < 10; ++i) {
+        SystemUser student = new SystemUser();
+        student.setUsername("student-" + i);
+        student.setPassword(passwordEncoder.encode("student-" + i + "-password"));
+        student.setRoles(Set.of("STUDENT"));
+        userRepository.save(student);
+      }
     }
+
+    if (labInfoRepository.count() == 0) {
+      LabInfo lab1 = new LabInfo();
+      lab1.setName("Data Lab");
+      lab1.setDescription(
+          "Students implement simple logical, two's complement, and floating point functions, but using a highly restricted subset of C. For example, they might be asked to compute the absolute value of a number using only bit-level operations and straightline code. This lab helps students understand the bit-level representations of C data types and the bit-level behavior of the operations on data.");
+      labInfoRepository.save(lab1);
+
+      LabInfo lab2 = new LabInfo();
+      lab2.setName("Bomb Lab");
+      lab2.setDescription("...");
+      labInfoRepository.save(lab2);
+    }
+  }
 }
--- management-system-backend/src/main/java/cc/blocklune/csapp/repository/LabInfoRepository.java
+++ management-system-backend/src/main/java/cc/blocklune/csapp/repository/LabInfoRepository.java
@@ -3,7 +3,6 @@ package cc.blocklune.csapp.repository;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import cc.blocklune.csapp.model.LabInfo;
-import java.util.Optional;
 
 @Repository
 public interface LabInfoRepository extends JpaRepository<LabInfo, Long> {
