a9204812a65f8d605c5ba3c094a91761514ad754
BlockLune <39331194+BlockLune@users.noreply.github.com>
2024-11-01 12:05:26 +0800

    fix: move some codes to reuse layout

--- management-system-frontend/app/teachers/layout.tsx
+++ management-system-frontend/app/teachers/layout.tsx
@@ -1,17 +1,53 @@
-import { Flex, Box } from "@radix-ui/themes";
+'use client';
+
+import { Card, Flex, Button, Box } from "@radix-ui/themes";
+import SideBar from "@/components/side-bar";
+import { useRouter } from "next/navigation";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const router = useRouter();
   return (
     <Flex width="100vw" height="100vh"
       align="center" justify="center"
       className="heropattern"
     >
       <Box maxWidth="768px">
-        {children}
+        <Card mx="4">
+          <Flex>
+            <SideBar>
+              <Flex direction="column" gap="4">
+                <Button
+                  variant="outline"
+                  style={{ cursor: "pointer" }}
+                  onClick={() => { router.push("/teachers") }}
+                >
+                  Dashboard
+                </Button>
+                <Button
+                  variant="outline"
+                  style={{ cursor: "pointer" }}
+                  onClick={() => { router.push("/teachers/students") }}
+                >
+                  Manage Students
+                </Button>
+                <Button
+                  variant="outline"
+                  style={{ cursor: "pointer" }}
+                  onClick={() => { router.push("/teachers/labs") }}
+                >
+                  Manage Labs
+                </Button>
+              </Flex>
+            </SideBar>
+            <Box p="6">
+              {children}
+            </Box>
+          </Flex>
+        </Card>
       </Box>
     </Flex>
   );
--- management-system-frontend/app/teachers/page.tsx
+++ management-system-frontend/app/teachers/page.tsx
@@ -1,46 +1,19 @@
-'use client';
-
-import { Card, Flex, Button, Text, Heading } from "@radix-ui/themes";
-import SideBar from "@/components/side-bar";
+import { Flex, Text, Heading } from "@radix-ui/themes";
 import Image from "next/image";
 import imgUndrawEducator from "@/assets/undraw-educator.svg"
-import { useRouter } from "next/navigation";
 
 export default function TeachersPage() {
-    const router = useRouter();
     return (
-        <Card mx="4">
-            <Flex>
-                <SideBar>
-                    <Flex direction="column" gap="4">
-                        <Button
-                            variant="outline"
-                            style={{ cursor: "pointer" }}
-                            onClick={() => { router.push("/teachers/students") }}
-                        >
-                            Manage Students
-                        </Button>
-                        <Button
-                            variant="outline"
-                            style={{ cursor: "pointer" }}
-                            onClick={() => { router.push("/teachers/labs") }}
-                        >
-                            Manage Labs
-                        </Button>
-                    </Flex>
-                </SideBar>
-                <Flex p="6" direction="column" justify="between" gap="8">
-                    <Flex direction="column" gap="2">
-                        <Heading as="h2" size="4">
-                            Welcome!
-                        </Heading>
-                        <Text>
-                            Please select the content you want to manage on the left
-                        </Text>
-                    </Flex>
-                    <Image src={imgUndrawEducator} alt="Educator" width={400} />
-                </Flex>
+        <Flex direction="column" justify="between" gap="8" height="100%">
+            <Flex direction="column" gap="2">
+                <Heading as="h2" size="4">
+                    Welcome!
+                </Heading>
+                <Text>
+                    Please select the content you want to manage on the left
+                </Text>
             </Flex>
-        </Card>
+            <Image src={imgUndrawEducator} alt="Educator" width={400} />
+        </Flex>
     );
 }
