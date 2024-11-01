92795945ff97185d18882e16f15470b2a205e209
BlockLune <39331194+BlockLune@users.noreply.github.com>
2024-11-01 12:44:12 +0800

    feat: add animation for teachers page

--- management-system-frontend/app/teachers/layout.tsx
+++ management-system-frontend/app/teachers/layout.tsx
@@ -3,6 +3,7 @@
 import { Card, Flex, Button, Box } from "@radix-ui/themes";
 import SideBar from "@/components/side-bar";
 import { useRouter } from "next/navigation";
+import * as motion from "framer-motion/client";
 
 export default function RootLayout({
   children,
@@ -15,39 +16,44 @@ export default function RootLayout({
       align="center" justify="center"
       className="heropattern"
     >
-      <Box maxWidth="768px">
-        <Card mx="4">
-          <Flex>
-            <SideBar>
-              <Flex direction="column" gap="4">
-                <Button
-                  variant="outline"
-                  style={{ cursor: "pointer" }}
-                  onClick={() => { router.push("/teachers") }}
-                >
-                  Dashboard
-                </Button>
-                <Button
-                  variant="outline"
-                  style={{ cursor: "pointer" }}
-                  onClick={() => { router.push("/teachers/students") }}
-                >
-                  Manage Students
-                </Button>
-                <Button
-                  variant="outline"
-                  style={{ cursor: "pointer" }}
-                  onClick={() => { router.push("/teachers/labs") }}
-                >
-                  Manage Labs
-                </Button>
-              </Flex>
-            </SideBar>
-            <Box p="6">
-              {children}
-            </Box>
-          </Flex>
-        </Card>
+      <Box width="768px" maxWidth="768px">
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+        >
+          <Card mx="4">
+            <Flex height="512px">
+              <SideBar>
+                <Flex direction="column" gap="4">
+                  <Button
+                    variant="outline"
+                    style={{ cursor: "pointer" }}
+                    onClick={() => { router.push("/teachers") }}
+                  >
+                    Dashboard
+                  </Button>
+                  <Button
+                    variant="outline"
+                    style={{ cursor: "pointer" }}
+                    onClick={() => { router.push("/teachers/students") }}
+                  >
+                    Manage Students
+                  </Button>
+                  <Button
+                    variant="outline"
+                    style={{ cursor: "pointer" }}
+                    onClick={() => { router.push("/teachers/labs") }}
+                  >
+                    Manage Labs
+                  </Button>
+                </Flex>
+              </SideBar>
+              <Box p="6">
+                {children}
+              </Box>
+            </Flex>
+          </Card>
+        </motion.div>
       </Box>
     </Flex>
   );
