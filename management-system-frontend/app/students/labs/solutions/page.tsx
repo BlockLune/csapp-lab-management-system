a768b6dbc15a27447b2e54a35f21e1ac8d0c245e
BlockLune <39331194+BlockLune@users.noreply.github.com>
2024-11-04 10:53:28 +0800

    feat: main nav of lab solutions

--- management-system-frontend/app/students/labs/solutions/page.tsx
+++ management-system-frontend/app/students/labs/solutions/page.tsx
@@ -1,5 +1,103 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { Flex, Table, Heading, Text, Skeleton, Link } from "@radix-ui/themes";
+import { type LabInfo, getLabList } from "@/scripts/api";
+
 export default function LabSolutions() {
-    return (
-        <p>Solutions</p>
-    )
+  const [fetching, setFetching] = useState<boolean>(true);
+  const [labs, setLabs] = useState<LabInfo[]>([]);
+
+  const fetchLabs = async () => {
+    setFetching(true);
+    const labs = await getLabList();
+    if (labs) {
+      setLabs(labs);
+    } else {
+      setLabs([]);
+    }
+    setFetching(false);
+  };
+
+  useEffect(() => {
+    fetchLabs();
+  }, []);
+
+  const tableSkeleton = (
+    <Table.Root style={{ width: "100%", overflowY: "auto" }}>
+      <Table.Header>
+        <Table.Row>
+          <Table.ColumnHeaderCell justify="center">
+            Lab Name
+          </Table.ColumnHeaderCell>
+          <Table.ColumnHeaderCell justify="center">
+            Materials
+          </Table.ColumnHeaderCell>
+        </Table.Row>
+      </Table.Header>
+      <Table.Body>
+        {Array.from({ length: 8 }).map((_, i) => {
+          return (
+            <Table.Row key={i}>
+              <Table.RowHeaderCell justify="center">
+                <Skeleton>BlockLune</Skeleton>
+              </Table.RowHeaderCell>
+              <Table.Cell justify="center">
+                <Skeleton>BlockLune</Skeleton>
+              </Table.Cell>
+            </Table.Row>
+          );
+        })}
+      </Table.Body>
+    </Table.Root>
+  );
+
+  const tableComponent = (
+    <Table.Root style={{ width: "100%", overflowY: "auto" }}>
+      <Table.Header>
+        <Table.Row>
+          <Table.ColumnHeaderCell justify="center">
+            Lab Name
+          </Table.ColumnHeaderCell>
+          <Table.ColumnHeaderCell justify="center">
+            Solutions
+          </Table.ColumnHeaderCell>
+        </Table.Row>
+      </Table.Header>
+      <Table.Body>
+        {labs.map((lab) => {
+          return (
+            <Table.Row key={lab.id}>
+              <Table.RowHeaderCell justify="center">
+                {lab.name}
+              </Table.RowHeaderCell>
+              <Table.Cell justify="center">
+                <Link href={`/students/labs/solutions/${lab.id}`}>Check</Link>
+              </Table.Cell>
+            </Table.Row>
+          );
+        })}
+      </Table.Body>
+    </Table.Root>
+  );
+
+  const fallbackComponent = <Text>No labs found</Text>;
+
+  return (
+    <Flex direction="column" gap="4" width="100%" height="100%">
+      <Flex direction="column" gap="2">
+        <Heading as="h2" size="4">
+          Lab Solutions
+        </Heading>
+        <Text>Check and manage your solutions here.</Text>
+      </Flex>
+      <Flex justify="center" height="80%">
+        {fetching
+          ? tableSkeleton
+          : labs.length !== 0
+          ? tableComponent
+          : fallbackComponent}
+      </Flex>
+    </Flex>
+  );
 }
--- management-system-frontend/app/teachers/labs/page.tsx
+++ management-system-frontend/app/teachers/labs/page.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import { useState, useEffect } from "react";
 import { Flex, Table, Heading, Text, Skeleton, Link } from "@radix-ui/themes";
@@ -8,46 +8,62 @@ export default function ManageLabsPage() {
   const [fetching, setFetching] = useState<boolean>(true);
   const [labs, setLabs] = useState<LabInfo[]>([]);
 
-  const fetchStudents = async () => {
+  const fetchLabs = async () => {
     setFetching(true);
-    const students = await getLabList();
-    if (students) {
-      setLabs(students);
+    const labs = await getLabList();
+    if (labs) {
+      setLabs(labs);
     } else {
       setLabs([]);
     }
     setFetching(false);
-  }
+  };
 
   useEffect(() => {
-    fetchStudents();
+    fetchLabs();
   }, []);
 
   const tableSkeleton = (
     <Table.Root style={{ width: "100%", overflowY: "auto" }}>
       <Table.Header>
         <Table.Row>
-          <Table.ColumnHeaderCell justify="center">Lab Id</Table.ColumnHeaderCell>
+          <Table.ColumnHeaderCell justify="center">
+            Lab Id
+          </Table.ColumnHeaderCell>
           <Table.ColumnHeaderCell justify="center">Name</Table.ColumnHeaderCell>
-          <Table.ColumnHeaderCell justify="center">Description</Table.ColumnHeaderCell>
-          <Table.ColumnHeaderCell justify="center">Materials</Table.ColumnHeaderCell>
-          <Table.ColumnHeaderCell justify="center">Solutions</Table.ColumnHeaderCell>
+          <Table.ColumnHeaderCell justify="center">
+            Description
+          </Table.ColumnHeaderCell>
+          <Table.ColumnHeaderCell justify="center">
+            Materials
+          </Table.ColumnHeaderCell>
+          <Table.ColumnHeaderCell justify="center">
+            Solutions
+          </Table.ColumnHeaderCell>
         </Table.Row>
       </Table.Header>
       <Table.Body>
-        {
-          Array.from({ length: 8 }).map((_, i) => {
-            return (
-              <Table.Row key={i}>
-                <Table.RowHeaderCell justify="center"><Skeleton>BlockLune</Skeleton></Table.RowHeaderCell>
-                <Table.Cell justify="center"><Skeleton>BlockLune</Skeleton></Table.Cell>
-                <Table.Cell justify="center"><Skeleton>BlockLune</Skeleton></Table.Cell>
-                <Table.Cell justify="center"><Skeleton>BlockLune</Skeleton></Table.Cell>
-                <Table.Cell justify="center"><Skeleton>BlockLune</Skeleton></Table.Cell>
-              </Table.Row>
-            );
-          })
-        }
+        {Array.from({ length: 8 }).map((_, i) => {
+          return (
+            <Table.Row key={i}>
+              <Table.RowHeaderCell justify="center">
+                <Skeleton>BlockLune</Skeleton>
+              </Table.RowHeaderCell>
+              <Table.Cell justify="center">
+                <Skeleton>BlockLune</Skeleton>
+              </Table.Cell>
+              <Table.Cell justify="center">
+                <Skeleton>BlockLune</Skeleton>
+              </Table.Cell>
+              <Table.Cell justify="center">
+                <Skeleton>BlockLune</Skeleton>
+              </Table.Cell>
+              <Table.Cell justify="center">
+                <Skeleton>BlockLune</Skeleton>
+              </Table.Cell>
+            </Table.Row>
+          );
+        })}
       </Table.Body>
     </Table.Root>
   );
@@ -56,43 +72,44 @@ export default function ManageLabsPage() {
     <Table.Root style={{ width: "100%", overflowY: "auto" }}>
       <Table.Header>
         <Table.Row>
-          <Table.ColumnHeaderCell justify="center">Lab Id</Table.ColumnHeaderCell>
+          <Table.ColumnHeaderCell justify="center">
+            Lab Id
+          </Table.ColumnHeaderCell>
           <Table.ColumnHeaderCell justify="center">Name</Table.ColumnHeaderCell>
-          <Table.ColumnHeaderCell justify="center">Description</Table.ColumnHeaderCell>
-          <Table.ColumnHeaderCell justify="center">Materials</Table.ColumnHeaderCell>
-          <Table.ColumnHeaderCell justify="center">Solutions</Table.ColumnHeaderCell>
+          <Table.ColumnHeaderCell justify="center">
+            Description
+          </Table.ColumnHeaderCell>
+          <Table.ColumnHeaderCell justify="center">
+            Materials
+          </Table.ColumnHeaderCell>
+          <Table.ColumnHeaderCell justify="center">
+            Solutions
+          </Table.ColumnHeaderCell>
         </Table.Row>
       </Table.Header>
       <Table.Body>
-        {
-          labs.map((lab) => {
-            return (
-              <Table.Row key={lab.id}>
-                <Table.RowHeaderCell justify="center">{lab.id}</Table.RowHeaderCell>
-                <Table.Cell justify="center">{lab.name}</Table.Cell>
-                <Table.Cell>{lab.description}</Table.Cell>
-                <Table.Cell justify="center">
-                  <Link href={`/teachers/labs/${lab.id}/materials`}>
-                    Manage
-                  </Link>
-                </Table.Cell>
-                <Table.Cell justify="center">
-                  <Link href={`/teachers/labs/${lab.id}/solutions`}>
-                    Check
-                  </Link>
-                </Table.Cell>
-              </Table.Row>
-            );
-          })
-        }
+        {labs.map((lab) => {
+          return (
+            <Table.Row key={lab.id}>
+              <Table.RowHeaderCell justify="center">
+                {lab.id}
+              </Table.RowHeaderCell>
+              <Table.Cell justify="center">{lab.name}</Table.Cell>
+              <Table.Cell>{lab.description}</Table.Cell>
+              <Table.Cell justify="center">
+                <Link href={`/teachers/labs/${lab.id}/materials`}>Manage</Link>
+              </Table.Cell>
+              <Table.Cell justify="center">
+                <Link href={`/teachers/labs/${lab.id}/solutions`}>Check</Link>
+              </Table.Cell>
+            </Table.Row>
+          );
+        })}
       </Table.Body>
     </Table.Root>
   );
 
-  const fallbackComponent = (
-    <Text>No labs found</Text>
-  );
-
+  const fallbackComponent = <Text>No labs found</Text>;
 
   return (
     <Flex direction="column" gap="4" width="100%" height="100%">
@@ -101,13 +118,16 @@ export default function ManageLabsPage() {
           Lab Management Panel
         </Heading>
         <Text>
-          You can check lab materials and students' solutions here. Contact the admin if you want to add or remove labs.
+          You can check lab materials and students' solutions here. Contact the
+          admin if you want to add or remove labs.
         </Text>
       </Flex>
       <Flex justify="center" height="80%">
-        {
-          fetching ? tableSkeleton : (labs.length !== 0 ? tableComponent : fallbackComponent)
-        }
+        {fetching
+          ? tableSkeleton
+          : labs.length !== 0
+          ? tableComponent
+          : fallbackComponent}
       </Flex>
     </Flex>
   );
