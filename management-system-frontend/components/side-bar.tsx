7693a92d54108395f1040ab4c9b5c22a69fe06d5
BlockLune <39331194+BlockLune@users.noreply.github.com>
2024-11-06 13:25:35 +0800

    fix: text & links on the side bar

--- management-system-frontend/components/side-bar.tsx
+++ management-system-frontend/components/side-bar.tsx
@@ -1,4 +1,5 @@
-import { Flex, Heading, Text, Box } from "@radix-ui/themes";
+import { Flex, Heading, Text, Box, Link, IconButton } from "@radix-ui/themes";
+import { GitHubLogoIcon } from "@radix-ui/react-icons";
 
 export default function SideBar({
   children,
@@ -6,25 +7,51 @@ export default function SideBar({
   children: React.ReactNode;
 }>) {
   return (
-    <Flex direction="column" p="6" justify="between" gap="6"
+    <Flex
+      direction="column"
+      p="6"
+      justify="between"
+      gap="6"
       style={{ borderRight: "1px solid #e5e5e5" }}
     >
       <Flex direction="column" flexGrow="1" gap="6">
         <Heading>
           <Box>
             <Box as="span">CSAPP</Box>
+            <Box as="span">Lab</Box>
             <Box as="span">Management</Box>
             <Box as="span">System</Box>
           </Box>
         </Heading>
-        <Box>
-          {children}
-        </Box>
+        <Box>{children}</Box>
       </Flex>
-      <Text>
-        Built by BlockLune
-      </Text>
-    </Flex >
+      <Flex justify="between" align="end">
+        <Flex direction="column">
+          <Text size="1">Powered by</Text>
+          <Text size="1">
+            <Link href="https://nextjs.org/" target="_blank" rel="nofollow">
+              NextJS
+            </Link>
+            {" & "}
+            <Link
+              href="https://www.radix-ui.com/"
+              target="_blank"
+              rel="nofollow"
+            >
+              Radix UI
+            </Link>
+          </Text>
+        </Flex>
+        <Link
+          href="https://github.com/BlockLune/csapp-lab-management-system"
+          target="_blank"
+          rel="nofollow"
+        >
+          <IconButton variant="ghost">
+            <GitHubLogoIcon />
+          </IconButton>
+        </Link>
+      </Flex>
+    </Flex>
   );
 }
-
